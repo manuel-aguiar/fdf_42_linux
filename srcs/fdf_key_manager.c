e03cf63d697cc824b933441bb212682dc80f109e
Manuel Maria Do Nascimento Aguiar <mmaria-d@c2r7s6.42lisboa.com>
2023-08-26 14:22:09 +0100

    buttons

--- srcs/fdf_key_manager.c
+++ srcs/fdf_key_manager.c
@@ -19,16 +19,16 @@ int	key_manager(t_fdf *fdf)
 	key = fdf->keys;
 	if ((key >> BIT_ESC) & 1)
 		free_fdf(fdf);
-	if ((key >> BIT_AU) & 1 || (key >> BIT_AD) & 1 || (key >> BIT_AR) & 1 || (key >> BIT_AL) & 1)
+	if ((key >> BIT_MVU) & 1 || (key >> BIT_MVD) & 1 || (key >> BIT_MVR) & 1 || (key >> BIT_MVL) & 1)
 		adjust_offset(fdf, key, 2);
-	if ((key >> BIT_O) & 1 || (key >> BIT_P) & 1)
+	if ((key >> BIT_ZIN) & 1 || (key >> BIT_ZOUT) & 1)
 		adjust_zoom(fdf, key, 1);
-	if ((key >> BIT_H) & 1 || (key >> BIT_L) & 1)
+	if ((key >> BIT_H_UP) & 1 || (key >> BIT_H_DOWN) & 1)
 		adjust_height(fdf, key, 0.002f);
-	if ((key >> BIT_A) & 1 || (key >> BIT_D) & 1 || (key >> BIT_S) & 1 \
-			|| (key >> BIT_W) & 1 || (key >> BIT_Q) & 1 || (key >> BIT_E) & 1)
+	if ((key >> BIT_RX_L) & 1 || (key >> BIT_RX_R) & 1 || (key >> BIT_RY_L) & 1 \
+			|| (key >> BIT_RY_R) & 1 || (key >> BIT_RZ_L) & 1 || (key >> BIT_RZ_R) & 1)
 		adjust_rotation(fdf, key, 0.004f);
-	if ((key >> BIT_B) & 1 || (key >> BIT_N) & 1 || (key >> BIT_M) & 1)
+	if ((key >> BIT_VISO) & 1 || (key >> BIT_VPLL) & 1 || (key >> BIT_VTOP) & 1)
 		apply_projection(fdf, key);
 	setup_vertices(fdf);
 	return (0);
@@ -36,7 +36,7 @@ int	key_manager(t_fdf *fdf)
 
 int	adjust_height(t_fdf *fdf, int key, float change)
 {
-	if ((key >> BIT_L) & 1)
+	if ((key >> BIT_H_DOWN) & 1)
 		change *= -1;
 	fdf->view.z_multi += change;
 	if (fdf->view.z_multi < 0.01f)
@@ -46,21 +46,21 @@ int	adjust_height(t_fdf *fdf, int key, float change)
 
 int	adjust_rotation(t_fdf *fdf, int key, float change)
 {
-	if ((key >> BIT_S) & 1 || (key >> BIT_D) & 1 || (key >> BIT_Q) & 1)
+	if ((key >> BIT_RY_L) & 1 || (key >> BIT_RX_R) & 1 || (key >> BIT_RZ_L) & 1)
 		change *= -1;
-	if ((key >> BIT_S) & 1|| (key >> BIT_W) & 1)
+	if ((key >> BIT_RY_L) & 1 || (key >> BIT_RY_R) & 1)
 	{
 		fdf->view.x_angle += change;
 		fdf->view.cos_x = cosf(fdf->view.x_angle);
 		fdf->view.sin_x = sinf(fdf->view.x_angle);
 	}
-	if ((key >> BIT_A) & 1 || (key >> BIT_D) & 1)
+	if ((key >> BIT_RX_L) & 1 || (key >> BIT_RX_R) & 1)
 	{
 		fdf->view.y_angle += change;
 		fdf->view.cos_y = cosf(fdf->view.y_angle);
 		fdf->view.sin_y = sinf(fdf->view.y_angle);
 	}
-	if ((key >> BIT_Q) & 1 || (key >> BIT_E) & 1)
+	if ((key >> BIT_RZ_L) & 1 || (key >> BIT_RZ_R) & 1)
 	{
 		fdf->view.z_angle += change;
 		fdf->view.cos_z = cosf(fdf->view.z_angle);
--- srcs/fdf_key_manager2.c
+++ srcs/fdf_key_manager2.c
@@ -14,13 +14,13 @@
 
 int	adjust_offset(t_fdf *fdf, int key, int change)
 {
-	if ((key >> BIT_AU) & 1)
+	if ((key >> BIT_MVU) & 1)
 		fdf->view.y_offset += -change;
-	if ((key >> BIT_AD) & 1)
+	if ((key >> BIT_MVD) & 1)
 		fdf->view.y_offset += change;
-	if ((key >> BIT_AR) & 1)
+	if ((key >> BIT_MVR) & 1)
 		fdf->view.x_offset += change;
-	if ((key >> BIT_AL) & 1)
+	if ((key >> BIT_MVL) & 1)
 		fdf->view.x_offset += -change;
 	return (1);
 }
@@ -30,7 +30,7 @@ int	adjust_zoom(t_fdf *fdf, int key, int change)
 	int	prev_zoom;
 
 	prev_zoom = fdf->view.zoom;
-	if ((key >> BIT_P) & 1)
+	if ((key >> BIT_ZOUT) & 1)
 		change *= -1;
 	fdf->view.zoom += change;
 	if (fdf->view.zoom < 1)
@@ -44,19 +44,19 @@ int	adjust_zoom(t_fdf *fdf, int key, int change)
 
 int	apply_projection(t_fdf *fdf, int key)
 {
-	if ((key >> BIT_B) & 1)
+	if ((key >> BIT_VISO) & 1)
 	{
 		fdf->view.x_angle = X_ANGLE_ISO * (MY_PI / 180.0);
 		fdf->view.y_angle = Y_ANGLE_ISO * (MY_PI / 180.0);
 		fdf->view.z_angle = Z_ANGLE_ISO * (MY_PI / 180.0);
 	}
-	if ((key >> BIT_N) & 1)
+	if ((key >> BIT_VPLL) & 1)
 	{
 		fdf->view.x_angle = X_ANGLE_PLL * (MY_PI / 180.0);
 		fdf->view.y_angle = Y_ANGLE_PLL * (MY_PI / 180.0);
 		fdf->view.z_angle = Z_ANGLE_PLL * (MY_PI / 180.0);
 	}
-	if ((key >> BIT_M) & 1)
+	if ((key >> BIT_VTOP) & 1)
 	{
 		fdf->view.x_angle = X_ANGLE_TOP * (MY_PI / 180.0);
 		fdf->view.y_angle = Y_ANGLE_TOP * (MY_PI / 180.0);
